18f9ca7d12b49fbf96a1a7da60c7506873b0400e
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2025-12-07 23:23:58 +0100

    Increased job number limit by default for download script.

--- scripts/download_month_files.py
+++ scripts/download_month_files.py
@@ -53,7 +53,7 @@ logger: logging.Logger = logging_common.initialize_logger()
 # %%
 # Get the Remote instances for the previously sent requests
 # %%
-remotes: dict[str, CachedRemote] = get_remotes()
+remotes: dict[str, CachedRemote] = get_remotes(limit=300)
 # %%
 remotes_dict: dict[VarSet, dict[YearMonth, CachedRemote]] = (
     remotes_dict_by_vars_and_yearmonth(remotes.values())
