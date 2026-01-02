92283e5ea3af0c334729bf87aee57b087f2b2af8
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2026-01-02 22:38:42 +0100

    Requested files for 1952-01 through 1959-12

--- scripts/request_month_files.py
+++ scripts/request_month_files.py
@@ -38,9 +38,9 @@ logger: logging.Logger = logging_common.initialize_logger()
 # %%
 years_months: list[YearMonth] = [
     YearMonth(year=_year, month=_month)
-    for _year in range(1960, 1967+1)
+    for _year in range(1952, 1959+1)
     for _month in range(1, 12+1)
-] + [YearMonth(year=1983, month=_month) for _month in (10, 11, 12)]
+]
 
 print(f'Requesting data for {len(years_months)} year-months:')
 print(',\n'.join(f'{ym.year:04d}-{ym.month:02d}' for ym in years_months))
