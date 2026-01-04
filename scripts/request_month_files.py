9c149a9670bd4e77241140e29c811ac060e97135
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2026-01-04 12:43:13 +0100

    Changed previous request from starting in 1948-01 to 1950-01.
    The dataset in the request starts in 1950, so the months before that
    failed.

--- scripts/request_month_files.py
+++ scripts/request_month_files.py
@@ -38,7 +38,7 @@ logger: logging.Logger = logging_common.initialize_logger()
 # %%
 years_months: list[YearMonth] = [
     YearMonth(year=_year, month=_month)
-    for _year in range(1948, 1951+1)
+    for _year in range(1950, 1951+1)
     for _month in range(1, 12+1)
 ]
 
